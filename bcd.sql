32bd1a0db7d273884a5d16fbfa3da6224a0e9b83
ObeliscoAtormentador1 <tamosdejesusjoaovitor@gmail.com>
2025-02-07 21:00:09 -0300

    arquivo do banco incluido

---
+++ bcd.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: data
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sensores`
+--
+
+DROP TABLE IF EXISTS `sensores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sensores` (
+  `idsensores` int NOT NULL AUTO_INCREMENT,
+  `temperatura` varchar(45) DEFAULT NULL,
+  `pressao` varchar(45) DEFAULT NULL,
+  `umidade` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idsensores`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sensores`
+--
+
+LOCK TABLES `sensores` WRITE;
+/*!40000 ALTER TABLE `sensores` DISABLE KEYS */;
+INSERT INTO `sensores` VALUES (1,'63.66','938.57','61.76'),(2,'27.31','1034.85','55.73'),(3,'26.15','1053.58','69.7'),(4,'34.48','904.1','75.09'),(5,'79.22','950.7','85.08'),(6,'52.21','986.76','79.53'),(7,'63.44','1043.92','43.67'),(8,'62.36','1074.18','59.94'),(9,'41.48','915.26','54.1'),(10,'79.18','901.43','80.37'),(11,'79.68','909.86','88.03'),(12,'25.01','990.37','88.79'),(13,'74.37','1004.71','53.56'),(14,'26.2','1085.92','40.05'),(15,'68.47','947.43','44.34'),(16,'35.26','955.0','81.01'),(17,'48.89','1018.73','46.58'),(18,'27.32','940.45','37.14');
+/*!40000 ALTER TABLE `sensores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-07 20:59:27
